59987346170f3977f774895f4ceb8cc2baf96f98
alex <realcrazyn@yandex.ru>
2021-06-03 13:50:50 +0300

    Finish base framework functional

---
+++ src/core/DomListener.js
@@ -0,0 +1,37 @@
+import {capitalise} from '@core/utils';
+
+export class DomListener {
+	constructor($root, listners = []) {
+		if (!$root) {
+			throw new Error('No #root provided for DomListener!')
+		}
+		this.$root = $root
+		this.listeners = listners
+	}
+	initDOMListners() {
+		this.listeners.forEach(listener => {
+			const method = getMethodName(listener)
+			if (!this[method]) {
+				const name = this.name || ''
+				throw new Error(
+					`Method ${method} is not implemented in ${name} Component`)
+			}
+			// Тоже самое что и AddEventLIstner но на root его повесить невозможно
+			// потому что это не наттивный??? элемент и метода такого у него нет
+			// а если в Дом создать метод on и там сделать AddEventLIstner все ок
+			this[method] = this[method].bind(this)
+			this.$root.on(listener, this[method])
+		})
+	}
+	removeDOMListners() {
+		this.listeners.forEach(listener => {
+			const method = getMethodName(listener)
+			this.$root.off(listener, this[method])
+		})
+	}
+}
+// inpit => onInput
+function getMethodName(eventName) {
+	return 'on' + capitalise(eventName)
+}
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-
+import {Excel} from '@/comonents/excel/Excel';
+import {Header} from '@/comonents/header/Header';
+import {Toolbar} from '@/comonents/toolbar/Toolbar';
+import {Formula} from '@/comonents/formula/Formula';
+import {Table} from '@/comonents/table/Table';
 import './scss/index.scss'
 
-console.log('Workinaaaaaaaag!')
+
+const excel = new Excel('#app', {
+	components: [Header, Toolbar, Formula, Table]
+})
+
+excel.render()
